ff78a9b7a1ac3d1ed299ab6a4e00f2bb777f9864
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-03-28 09:44:58 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
     switch(action.type){
         case "ADD_FRIEND":
-        return {"friends": [...state.friends, {name: 'Joe', hometown: 'Boston', id: 101}]
+        return {"friends": [...state.friends, action.friend]
         }
         case "REMOVE_FRIEND":
         return {"friends": [...state.friends].filter(friend=> friend.id!==action.id)}
